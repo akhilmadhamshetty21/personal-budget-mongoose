5c7839e1d7007cdaf0da421285a6a4747ee09279
akhil <akhilmadhamshetty21@gmail.com>
2020-10-28 20:22:54 -0400

    Mongoose added

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 //providing data
 
 const express = require("express");
+var mongoose=require("mongoose");
 const cors = require("cors")
 const app = express();
-const port = 3000;
+const routes=require("./routes");
+const port = 4000;
+var url = "mongodb://localhost:27017/budget";
+var budgetModel=require('./models/budget_schema');
 const budget = require("./data");
+const router = express.Router()
+mongoose.connect(url,{useNewUrlParser:true, useUnifiedTopology:true})
+        .then(()=>{
+            app.use(express.json())
+            app.use("/api",routes)
+        })
+        .catch((connectionError)=>{
+          console.log(connectionError)
+        })
 app.use(cors());
 app.get("/budget", (req, res) => {
   res.json(budget);
